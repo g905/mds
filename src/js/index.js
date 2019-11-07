d7f7fc46426a990f787e6884290c8005d6570d52
g905 <breakestra@yandex.ru>
2019-11-07 18:19:08 +0400

    init commit

---
+++ src/js/index.js
@@ -0,0 +1,326 @@
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/js/dist/util';
+import AOS from 'aos'
+import Pace from 'pace-js/pace'
+window.jQuery = $;
+window.$ = $;
+//require("@fancyapps/fancybox");
+import 'slick-carousel'
+//import List from 'list.js'
+/*import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faViber, faSkype, faWhatsapp, faVk, faYoutube, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faMapMarkerAlt, faSearch, faChevronDown, faChevronLeft, faChevronRight, faTh, faThList } from '@fortawesome/free-solid-svg-icons'
+library.add( faViber, faSkype, faWhatsapp, faVk, faYoutube, faFacebook, faInstagram, faMapMarkerAlt, faSearch, faChevronDown, faChevronLeft, faChevronRight, faTh, faThList );
+
+// ====================================== Replace fa-icons with SVGs ===========================
+dom.watch();*/
+
+$(()=>{
+    Pace.start();
+    //console.log('loaded!!!');
+// ====================================== Animate on scroll ====================================
+
+    AOS.init();
+
+// ====================================== Loader ===============================================
+
+    //$('.loader-area').fadeOut().end().delay(400).fadeOut('slow');
+
+// ====================================== Navbar icon animation ================================
+
+    $('.nav-button').on('click', function () {
+        $('.animated-icon1').toggleClass('open');
+    });
+
+// ===================================== Custom File Input =====================================
+
+    $('#customFile').change((e)=>{
+        let filename = $(e.target).val();
+        filename = filename.substring(filename.lastIndexOf("\\") + 1, filename.length);
+        $(e.target).next('.custom-file-label').html(filename);
+    });
+
+// =================================== scroll to top button ====================================
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.scrollTop').fadeIn();
+        } else {
+            $('.scrollTop').fadeOut();
+        }
+    });
+    $('.scrollTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+
+// ================================== View of Items in Catalog =================================
+
+    function displayItems(){
+        let sideBarActive = window.localStorage.getItem('sidebar');
+        let viewItems = window.localStorage.getItem('view');
+        if(sideBarActive === 'true') {
+            $('.custom-switch input').attr('checked', true);
+            $('.left-bar').removeClass('collapsed');
+        } else {
+            $('.custom-switch input').attr('checked', false);
+            $('.left-bar').addClass('collapsed');
+            $('.left-bar .collapsible').removeClass('show');
+            $('.accordion-card').removeClass('active');
+        }
+
+        $('.view').removeClass('active');
+
+        if(viewItems === 'list'){
+            $('.catalog-items').addClass('list-view');
+            $('[data-view = list]').addClass('active');
+        } else {
+            $('.catalog-items').removeClass('list-view');
+            $('[data-view = tile]').addClass('active');
+        }
+
+        if(window.innerWidth <= 768) {
+            $('.custom-tile').css('display', 'none');
+            $('.catalog-items').removeClass('list-view');
+        }
+    }
+
+    displayItems();
+
+    $('.view').click((e) => {
+        if($(e.target).closest('div').hasClass('active')) {
+            return false;
+        }
+        let viewItem = $(e.target).closest('div');
+        let viewType = viewItem.data('view');
+        $('.view').removeClass('active');
+        viewItem.addClass('active');
+        if(viewType === 'list'){
+            $('.catalog-items').animate({'opacity': 0}, 200, function(){
+                $('.catalog-items').addClass('list-view');
+            });
+            $('.catalog-items').animate({'opacity': 1}, 200);
+            window.localStorage.setItem('view', 'list');
+        } else {
+            $('.catalog-items').animate({'opacity': 0}, 200, function(){
+                $('.catalog-items').removeClass('list-view');
+            });
+            $('.catalog-items').animate({'opacity': 1}, 200);
+            window.localStorage.setItem('view', 'tile');
+        }
+    });
+
+// ======================================== Calculator =========================================
+    let closeCalc = ()=>{
+
+        $('#calculator').removeClass('calc-show');
+        $('.screen').fadeOut();
+        $('#calculator, .filters, #faq #question').find('.active').removeClass('active');
+    };
+
+    $('.rightBtn').click((e)=>{
+        if(!$('#calculator').hasClass('calc-show')) {
+            $('#calculator').addClass('calc-show');
+            $('.screen').fadeIn();
+        } else {
+            return true;
+        }
+    });
+    $('.close-btn').click(closeCalc);
+
+    $('.screen').click(closeCalc);
+
+// ====================================== Feedback-form ========================================
+
+    $('.form-wrap form').submit((e) => {
+        $('.form-wrap, .backside').toggleClass('flipped');
+        e.preventDefault();
+    });
+
+// ========================================= Switcher ==========================================
+
+    $('.custom-switch input').click((e) => {
+        console.log(e.target.checked);
+        if(e.target.checked) {
+            $('.left-bar').removeClass('collapsed');
+        } else {
+            $('.left-bar').addClass('collapsed');
+            $('.left-bar .collapsible').removeClass('show');
+            $('.accordion-card').removeClass('active');
+        }
+        window.localStorage.setItem('sidebar', e.target.checked);
+    });
+
+// ======================================== Accordion ==========================================
+    $('#faq .accordion-card').click(function(e){
+        $(this).find('.collapsible').collapse('toggle');
+        $(this).toggleClass('active');
+    });
+
+// ====================================== Slick sliders ========================================
+
+    let slickCarousel = {
+        useTransform: true,
+        infinite: true,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        slidesToShow: 1,
+        cssEase: "ease-out",
+        appendArrows: $('#slickArrowsCarousel'),
+        prevArrow: $('#slickArrowsCarousel .prev'),
+        nextArrow: $('#slickArrowsCarousel .next')
+    };
+
+    let slickSolutions = {
+        infinite: false,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        appendArrows: $('#slickArrowsSolutions'),
+        prevArrow: $('#slickArrowsSolutions .prev'),
+        nextArrow: $('#slickArrowsSolutions .next'),
+        responsive: [{
+            breakpoint: '768',
+            settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 1
+            }
+        }]
+    };
+
+    let slickTeam = {
+        infinite: false,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button class="slick-arrow next"><i class="fas fa-chevron-right"></i></button>',
+        responsive: [{
+            breakpoint: '768',
+            settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 1
+            }
+        }]
+    };
+
+    let slickNews = {
+        infinite: false,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        appendArrows: $('#arrowsNews'),
+        prevArrow: $('#arrowsNews .prev'),
+        nextArrow: $('#arrowsNews .next'),
+        responsive: [{
+            breakpoint: '768',
+            settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 1
+            }
+        }]
+    };
+
+    let slickArticles = {
+        infinite: false,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        appendArrows: $('#arrowsArticles'),
+        prevArrow: $('#arrowsArticles .prev'),
+        nextArrow: $('#arrowsArticles .next'),
+        responsive: [{
+            breakpoint: '768',
+            settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 1
+            }
+        }]
+    };
+
+    let slickPartners = {
+        infinite: false,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        appendArrows: $('#arrowsPartners'),
+        prevArrow: $('#arrowsPartners .prev'),
+        nextArrow: $('#arrowsPartners .next'),
+        responsive: [{
+            breakpoint: '768',
+            settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 1
+            }
+        }]
+    };
+
+    let slickHistory = {
+        infinite: false,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendArrows: $('#arrowsHistory'),
+        prevArrow: $('#arrowsHistory .prev'),
+        nextArrow: $('#arrowsHistory .next'),
+        responsive: [{
+            breakpoint: '768',
+            settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 1
+            }
+        }]
+    };
+
+    let slickRelated = {
+        infinite: false,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        appendArrows: $('#arrowsRelated'),
+        prevArrow: $('#arrowsRelated .prev'),
+        nextArrow: $('#arrowsRelated .next'),
+        responsive: [{
+            breakpoint: '768',
+            settings: {
+                arrows: false,
+                dots: true,
+                slidesToShow: 1
+            }
+        }]
+    };
+
+    $('.carousel').slick(slickCarousel);
+    $('.slick-solutions').slick(slickSolutions);
+    $('.slick-team').slick(slickTeam);
+    $('.slick-news').slick(slickNews);
+    $('.slick-articles').slick(slickArticles);
+    $('.slick-partners').slick(slickPartners);
+    $('.slick-history').slick(slickHistory);
+    $('.slick-related').slick(slickRelated)
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,171 @@
+const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    optimization: {
+      minimizer: [
+          new UglifyJsPlugin({
+              cache: true,
+              parallel: true,
+              sourceMap: true
+          }),
+          new OptimizeCSSAssetsPlugin({})
+      ],
+        runtimeChunk: {name: 'common'},
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                commons: {
+                    test: /\.jsx?$/,
+                    chunks: 'all',
+                    minChunks: 2,
+                    name: 'common',
+                    enforce: true,
+                },
+            },
+        },
+
+    },
+    entry: [
+        './src/js/index.js',
+        './src/sass/style.sass'
+    ],
+    output: {
+        filename: './js/bundle.js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src/js'),
+                exclude: "/node_modules/",
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, 'src/sass'),
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            url: false
+                        },
+                    },
+                        {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    overrideBrowserslist: ['ie >= 8', 'last 4 versions']
+                                })
+                            ],
+                            sourceMap: true
+                        }
+
+                    },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+
+                            }
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.pug$/,
+                use: [{
+                    loader: 'pug-loader',
+                    options: {
+                        pretty: true
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|ttf)$/,
+                use: {
+                    loader: 'url-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: './css/style.bundle.css',
+            allChunks: true
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/fonts',
+            to: './fonts'
+        },
+            {
+                from: './src/img/favicon',
+                to: './favicon'
+            },
+            {
+                from: './src/img',
+                to: './img'
+            }
+        ]),
+        new ImageminPlugin({
+            pngquant: {
+                quality: '50-50'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/pug/views/index.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'main.html',
+            template: './src/pug/views/main.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'about.html',
+            template: './src/pug/views/about.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'services.html',
+            template: './src/pug/views/services.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'service.html',
+            template: './src/pug/views/service.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'projects.html',
+            template: './src/pug/views/projects.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'info.html',
+            template: './src/pug/views/info.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'info-detail.html',
+            template: './src/pug/views/info_detail.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'feedback.html',
+            template: './src/pug/views/feedback.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'contacts.html',
+            template: './src/pug/views/contacts.pug'
+        }),
+    ]
+};
